3cd402e7b82a3b3a88425f5e54f23eb139dd4c23
Zeming <chenz16@rose-hulman.edu>
2020-12-23 01:43:39 -0600

    feat:lexical knowledge filtering complete

--- src/wordnet.py
+++ src/wordnet.py
@@ -3,29 +3,7 @@ import requests
 import json
 import spacy
 import itertools
-
-import torch
-import torchtext.vocab as vocab
 from nltk.corpus import wordnet
-
-cache_dir = "../glove"
-glove = vocab.GloVe(name='6B', dim=50, cache=cache_dir)
-
-
-def closest(vec, n=10):
-    """
-    Find the closest words for a given vector
-    """
-    all_dists = [(w, torch.dist(vec, get_word(w))) for w in glove.itos]
-    return sorted(all_dists, key=lambda t: t[1])[:n]
-
-
-def get_dist(w1, w2):
-    return torch.dist(get_word(w1), get_word(w2))
-
-
-def get_word(word):
-    return glove.vectors[glove.stoi[word]]
 
 
 def find_relation(word1, word2, pos, verbose=0):
@@ -66,7 +44,8 @@ class ConceptNet:
     def relation(self, concept, rel='IsA'):
         hypernyms = set([])
         url_to_search = self.url + "query?start=/c/en/" + \
-            concept + "&rel=/r/" + rel + "&limit=1000"
+            concept + "&rel=/r/" + rel + "&limit=500"
+        print(url_to_search)
         data = urllib.request.urlopen(url_to_search)
         json_data = json.load(data)
         for edge in json_data["edges"]:
@@ -76,7 +55,8 @@ class ConceptNet:
                     hypernyms.add(edge['end']['label'])
 
         url_to_search = self.url + "query?end=/c/en/" + \
-            concept + "&rel=/r/" + rel + "&limit=1000"
+            concept + "&rel=/r/" + rel + "&limit=500"
+        print(url_to_search)
         data = urllib.request.urlopen(url_to_search)
         json_data = json.load(data)
         hyponyms = set([])
@@ -97,7 +77,6 @@ def get_word_sets(word, pos):
         for x in syn.hypernyms():
             for l in x.lemmas():
                 hypernyms.add(l.name().replace('_', ' '))
-                get_dist
         for x in syn.hyponyms():
             for l in x.lemmas():
                 hyponyms.add(l.name().replace('_', ' '))
